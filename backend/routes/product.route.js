e07806acdf2da0cf89e5b896127724fb4dec86b8
ImalkaPerera <91441333+ImalkaPerera@users.noreply.github.com>
2025-09-01 11:53:28 +0530

    created api endpoints

---
+++ backend/routes/product.route.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import Product from '../../models/Product.model.js';
+
+const router=express.Router();
+router.post("/products",async (req,res)=>{
+    const product=req.body;
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({success:false,message:"Please provide all fields"});
+    }
+    const newProduct=new Product(product);
+    try{
+        await newProduct.save();
+        res.status(201).json({success:true,message:"Product created successfully",data:newProduct});
+    }catch(error){
+        res.status(500).json({success:false,message:"Server Error"});   
+    }});
+router.delete("/api/products/:id",async (req,res)=>{
+    const {id}=req.params;
+    try{
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success:true,message:"Product deleted successfully"});
+    }catch(error){
+        res.status(500).json({success:false,message:"Server Error"});
+    }
+});
+router.get("/products",async(req,res)=>{
+    try{
+        const products=await Product.find({});
+        res.status(200).json({success:true,data:products});
+    }catch(error){
+        res.status(500).json({success:false,message:"Server Error"});
+    }
+})
+router.put("/api/products/:id",async(req,res)=>{
+    const {id}=req.params;
+    const product=req.body;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({success:false,message:"Invalid product ID"});
+    }
+    try{
+        const updatedProduct=await Product.findByIdAndUpdate(id,product,{new:true});
+        res.status(200).json({success:true,message:"Product updated successfully",data:updatedProduct});
+    }catch(error){
+        res.status(500).json({success:false,message:"Server Error"});
+    }
+})
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import { connectDB } from './config/db.js';
+import Product from '../models/Product.model.js';
+import router from './routes/product.route.js';
+
+
 
 const app=express();
+app.use(express.json());
+
+app.use(router)
 app.listen(5000,()=>{
+    connectDB();
     console.log("Server is running on port 5000");
 })
